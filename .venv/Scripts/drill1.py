a3bd26abb9df36e32fb71daadc4f13bc8fc2ae81
popopocorn <popcorn375@naver.com>
2024-09-22 02:23:11 +0900

    완료

--- .venv/Scripts/drill1.py
+++ .venv/Scripts/drill1.py
@@ -3,24 +3,26 @@ from turtle import *
 shape("turtle")
 def go_up():
     setheading(90)
-    forward(100)
+    forward(50)
     stamp()
 def go_down():
     setheading(270)
-    forward(100)
+    forward(50)
     stamp()
 def go_left():
     setheading(180)
-    forward(100)
+    forward(50)
     stamp()
 def go_right():
     setheading(0)
-    forward(100)
+    forward(50)
     stamp()
-
-onkeypress(go_up,"Up")
-onkeypress(go_down, "Down")
-onkeypress(go_left, "Left")
-onkeypress(go_right, "Right")
+def tutle_reset():
+    reset()
+onkey(go_up,"w")
+onkey(go_down, "s")
+onkey(go_left, "a")
+onkey(go_right, "d")
+onkey(tutle_reset, "Escape")
 listen()
 mainloop()
